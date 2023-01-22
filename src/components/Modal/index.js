879e6d7b37bc40801a866fc5ea9126cde52fa6fc
galenomoon <gui.galenocover@gmail.com>
2023-01-22 12:53:23 -0300

    MVP ready

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,18 +1,33 @@
 import { View, Text, TouchableOpacity as TO, Modal as MO, TextInput } from 'react-native'
 import React from 'react'
 
+//styles
 import tw from 'twrnc'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function Modal({ visible, type, close, onConfirm = () => { }, current_wallet_value, credit_limit }) {
+export default function Modal({ visible, type, close, onConfirm = () => { }, current_wallet_value, credit_limit, setCreditLimit }) {
   const [value, setValue] = React.useState(0)
   const input_ref = React.useRef(null)
+  const [final_value, setFinalValue] = React.useState(0)
 
   React.useEffect(() => {
     if (visible) {
+      setValue(0)
       input_ref.current.focus()
     }
   }, [visible])
+
+  React.useEffect(() => {
+    if (type === 'payment') {
+      setFinalValue(current_wallet_value - parseInt(value || 0))
+    }
+    if (type === 'receive') {
+      setFinalValue(current_wallet_value + parseInt(value || 0))
+    }
+    if (type === 'limit_setting') {
+      setFinalValue(parseInt(value || 0))
+    }
+  }, [value])
 
   return (
     <MO animationType="fade" transparent={true} visible={visible}>
@@ -21,27 +36,46 @@ export default function Modal({ visible, type, close, onConfirm = () => { }, cur
           <MaterialCommunityIcons name="close" color={'#777'} size={40} />
         </TO>
         <View style={tw`w-full h-full flex px-2 pt-[100px]`}>
-          <Text style={tw`text-2xl font-semibold`}>Quanto será {type === 'payment' ? 'pago' : 'recebido'}?</Text>
+          <Text style={tw`text-2xl font-semibold`}>
+            {type === "limit_setting" ?
+              'Qual será o novo limite de crédito (dívida)?' :
+              <>
+                Quanto será {type === 'payment' ? 'pago' : 'recebido'}?
+              </>
+            }
+          </Text>
           <View style={tw`flex flex-row mt-6`}>
             <Text style={tw`text-5xl font-thin`}>R$ </Text>
             <TextInput
               ref={input_ref}
-              onChangeText={text => setValue(text)}
-              value={value}
+              onChangeText={text => type === "limit_setting" ? setCreditLimit(parseInt(text)) : setValue(text)}
+              value={type === "limit_setting" ? `${credit_limit}` : value}
               style={tw`w-full text-5xl font-bold h-[58px]`}
               placeholder="00,00"
               maxLength={7}
               keyboardType='numeric'
             />
           </View>
-          <Text style={tw`text-xl font-semibold ${parseFloat(value) ? (credit_limit < parseInt(value || 0) && type === 'payment' ? "text-red-500" : "text-green-500") : "text-gray-500"}`}>
-            Valor final: R$ {type === 'payment' ? (current_wallet_value - parseInt(value || 0)) : (current_wallet_value + parseInt(value || 0))},00
-          </Text>
-          <TO onPress={onConfirm} style={tw`my-4 w-full h-[64px] ${parseFloat(value) ? type === 'payment' ? "bg-[#D42b15]" : "bg-[#34BE82]" : "bg-[#aaa]"} rounded-[8px] flex items-center justify-center `}>
-            <Text style={tw`text-xl uppercase text-white font-semibold`}>Confirmar</Text>
-          </TO>
+          {type !== "limit_setting" ?
+            <>
+              <Text style={tw`text-xl font-bold ${parseFloat(value) ? final_value < 0 ? "text-red-500" : "text-green-500" : "text-gray-500"}`}>
+                <Text style={tw`font-normal`}>Valor final:</Text> R$ {final_value},00
+              </Text>
+              <TO onPress={() => value ? [onConfirm(final_value), close()] : {}} style={tw`my-4 w-full h-[64px] ${parseFloat(value) ? type === 'payment' ? "bg-[#D42b15]" : "bg-[#34BE82]" : "bg-[#aaa]"} rounded-[8px] flex items-center justify-center `}>
+                <Text style={tw`text-xl uppercase text-white font-semibold`}>Confirmar</Text>
+              </TO>
+              {
+                final_value <= (credit_limit * (-1)) && type === 'payment' ?
+                  <Text style={tw`text-3xl text-center text-red-500`}>
+                    LIMITE ATINGIDO!
+                  </Text>
+                  : null
+              }
+            </>
+            : null
+          }
         </View>
       </View>
-    </MO>
+    </MO >
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,81 @@
-import { Image, Text, View, TouchableOpacity as TO } from 'react-native'
+import { Image, Text, View, TouchableOpacity as TO, Alert } from 'react-native'
 import React from 'react'
 
+//styles
 import tw from 'twrnc'
 import { LinearGradient } from 'expo-linear-gradient';
-import Modal from '../../components/Modal'
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ConfettiCannon from 'react-native-confetti-cannon';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import boat from '../../../assets/characters/boat.png'
-import car from '../../../assets/characters/car.jpeg'
-import cat from '../../../assets/characters/cat.jpg'
-import dog from '../../../assets/characters/dog.jpg'
-import dino from '../../../assets/characters/dino.jpg'
-import hat from '../../../assets/characters/hat.jpg'
-import penguin from '../../../assets/characters/penguin.jpg'
-import duck from '../../../assets/characters/duck.jpg'
+//components
+import Modal from '../../components/Modal'
 
-export default function Home({ navigation }) {
-  const [credit, setCredit] = React.useState(0)
+//utils
+import avatars from '../../utils/characters';
+
+export default function Home() {
   const [credit_limit, setCreditLimit] = React.useState(1500)
   const [debit, setDebit] = React.useState(1500)
   const [modalVisible, setModalVisible] = React.useState({ visible: false, type: '' })
+  const [showAvatars, setShowAvatars] = React.useState(false)
+  const [avatar_index, setAvatarIndex] = React.useState(0)
+  const confetti = React.useRef(null)
+
+  function resetGame() {
+    return Alert.alert('Iniciar nova partida?', 'Todos os dados serão perdidos e retornaram ao seu valor inicial', [
+      {
+        text: 'Cancel',
+        onPress: () => { },
+        style: 'cancel',
+      },
+      {
+        text: 'OK', onPress: () => {
+          setModalVisible({ type: '', visible: false })
+          setDebit(1500)
+        }
+      },
+    ]);
+  }
+
+  React.useEffect(() => {
+    if (!modalVisible.visible && modalVisible.type === 'receive') {
+      confetti.current.start()
+    }
+  }, [modalVisible, debit])
 
   return (
     <LinearGradient colors={['#ff5555', '#eb1020', '#bc0616']} style={tw`flex items-center h-full w-full relative`} >
+      <ConfettiCannon ref={confetti} autoStart={false} fadeOut={true} count={150} origin={{ x: 50, y: -10 }} />
       <Image source={require('../../../assets/moneys.png')} style={tw`absolute top-[85px] opacity-15 z-0`} />
-      <View style={tw`h-[30%] w-full flex items-center justify-center`}>
-        <Image source={require('../../../assets/logo.png')} style={tw`w-[320px] h-28 z-10`} />
+      <View style={tw`h-[30%] w-full flex flex-row items-center justify-center`}>
+        <TO onPress={() => setShowAvatars(!showAvatars)} style={tw`absolute top-12 right-5`} >
+          <Feather name={showAvatars ? "eye" : "eye-off"} size={32} color="#fff" />
+        </TO>
+        {showAvatars
+          ?
+          <View style={tw`flex flex-row items-center justify-center h-full`}>
+            <TO onPress={() => setAvatarIndex(avatar_index - 1)}>
+              <Ionicons name='chevron-back' size={64} color='#fff' />
+            </TO>
+            <Image source={avatars[avatar_index]} style={tw`w-[150px] h-[150px] z-10 mx-6 rounded-full`} />
+            <TO onPress={() => setAvatarIndex(avatar_index + 1)}>
+              <Ionicons name='chevron-forward' size={64} color='#fff' />
+            </TO>
+          </View>
+          :
+          <Image source={require('../../../assets/logo.png')} style={tw`w-[320px] h-28 z-10`} />
+        }
       </View>
-      <View style={tw`w-[95%] bg-[#fff] rounded-[24px] p-8`}>
+      <View style={tw`w-[95%] bg-[#fff] rounded-[24px] p-8 relative`}>
+        <TO onPress={() => resetGame()} style={tw`absolute top-4 right-4`}>
+          <Ionicons name="ios-reload" size={32} color="#000" />
+        </TO>
         <View style={tw`flex mb-[22px]`}>
           <Text style={tw`text-2xl font-semibold`}>Conta</Text>
-          <Text style={tw`text-2xl font-semibold mt-[12px]`}>R$ {debit.toFixed(2)}</Text>
+          <Text style={tw`text-2xl font-semibold mt-[12px]`}>R$ {debit > 0 ? debit.toFixed(2) : "00,00"}</Text>
         </View>
         <View style={tw`flex w-full justify-between items-center pb-[32px] border-b-[2px] border-[#ddd]`}>
           <TO onPress={() => setModalVisible({ visible: true, type: 'payment' })} style={tw`rounded-xl mb-2 flex flex-row w-full border-[#D42B15] border-[3px] h-[72px] items-center justify-center bg-[#F9C7C1]`}>
@@ -47,22 +92,30 @@ export default function Home({ navigation }) {
           </TO>
         </View>
         <View style={tw`flex my-[22px]`}>
-          <Text style={tw`text-2xl font-semibold`}>Dívida com o Banco</Text>
+          <View style={tw`flex flex-row items-center justify-between`}>
+            <Text style={tw`text-2xl font-semibold`}>Dívida com o Banco</Text>
+            <TO onPress={() => setModalVisible({ visible: true, type: 'limit_setting' })}>
+              <MaterialCommunityIcons name="credit-card-edit" size={32} color="#000" />
+            </TO>
+          </View>
           <View style={tw`mt-[12px]`}>
             <Text style={tw`text-lg font-semibold text-black/40`}>Atualmente devendo:</Text>
-            <Text style={tw`text-2xl font-semibold mt-1`}>R$ {credit.toFixed(2)}</Text>
-            <Text style={tw`text-lg font-semibold text-black/40 mt-1`}>Limite disponível de
-              <Text style={tw`text-[#D42b15] font-extrabold`}> R${credit_limit.toFixed(2)}</Text>
-            </Text>
+            <Text style={tw`text-2xl font-semibold mt-1`}>R$ {debit < 0 ? debit.toFixed(2) : '00,00'}</Text>
+            <View style={tw`flex flex-row items-center`}>
+              <Text style={tw`text-lg font-semibold text-black/40 mt-1`}>Limite disponível de</Text>
+              <Text style={tw`text-lg text-[#D42b15] font-extrabold`}> R${credit_limit.toFixed(2)}</Text>
+            </View>
           </View>
         </View>
       </View>
       <Modal
-        current_wallet_value={debit - credit}
+        current_wallet_value={debit}
         type={modalVisible.type}
         close={() => setModalVisible({ ...modalVisible, visible: false })}
         visible={modalVisible.visible}
         credit_limit={credit_limit}
+        setCreditLimit={(value) => setCreditLimit(value)}
+        onConfirm={(value) => setDebit(value)}
       />
     </LinearGradient>
   )
